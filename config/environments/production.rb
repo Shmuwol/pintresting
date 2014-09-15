a785f4df559e18460d35027c40c15d06aae2cbe9
Shmuwol <shmullywolfson@gmail.com>
2014-09-15 13:58:23 -0400

    Implemented Devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Required for Heroku
+  # Note to set for actual host
+  config.action_mailer.default_url_options = { :host => 'http://shmuwol-pinteresting.herokuapp.com/'  }
 end
